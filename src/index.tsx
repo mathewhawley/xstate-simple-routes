bffdf4a78240a23ffadcc9ce7e31d2e5e5a35647
Mathew Hawley <nimblepeanut@protonmail.com>
2020-07-11 12:33:29 +1200

    Add normalize.css and tachyons

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
+import 'tachyons';
 import App from 'components/App';
 import * as serviceWorker from './serviceWorker';
 
